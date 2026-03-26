4a13a4f1c5cb420afadb676fb8dcd5d85852c99a
Kristofer Reynolds <kristofertreynolds@gmail.com>
2026-03-26 23:20:52 +0000

    added fetch data and useEffect

--- src/js/components/Tasks.jsx
+++ src/js/components/Tasks.jsx
@@ -1,30 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Tasks = () => {
     
     const [tasks, setTasks] = useState([]);
     const [userInput, setUserInput] = useState("");
     
-    function updateTasks(task,index){
-        {tasks.map((task, index)=>{
-            //console.log(tasks);
-            return (
-                <li 
-                    onMouseOver={(event)=>{
-                        console.log("i hovered mouse", task, index);
-                        //return <h1>test</h1>;
-                    }}
-                    onClick={(event)=>{
-                        console.log("you clicked", task, index);
-                        //tasks.pop.task;
-                        tasks.splice(index,1);
+    useEffect(()=>{
+        //console.log("loading for first time");
 
-                    }}
-                    key={index}>{task}
-                </li>
-            );
-        })}
-    }
+        // fetch('https://playground.4geeks.com/todo/users/kristofer', {
+        //     method: "GET",
+        // }).then((response)=>{
+        //     console.log(response);
+        // }).then((data)=>{
+        //     console.log(data);
+        // });
+        
+        fetch('https://playground.4geeks.com/todo/users/kristofer', {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+        })
+        .then(resp => {
+            console.log(resp.ok); // Will be true if the response is successful
+            console.log(resp.status); // Status code 201, 300, 400, etc.
+            return resp.json(); // Will attempt to parse the result to JSON and return a promise where you can use .then to continue the logic
+        })
+        .then(data => {
+            // This is where your code should start after the fetch is complete
+            console.log(data); // This will print the exact object received from the server to the console
+            console.log(data.name);
+            console.log(data.todos);
+            console.log(data.todos[0].label);
+
+            const todolist = data.todos;
+            const todoArray = [];
+            for(let key in todolist){
+                todoArray.push(todolist[key].label);
+            }
+
+            setTasks(todoArray);
+        })
+        .catch(error => {
+            // Error handling
+            console.log(error);
+        });
+        
+    },[]);
     
     return(
 
